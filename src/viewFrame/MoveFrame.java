62a752f7d29dd017d4dae3d5d9307c371642125a
csw823197541 <cx123456>
2016-01-22 16:58:32 +0800

    指令表排序

--- src/viewFrame/MoveFrame.java
+++ src/viewFrame/MoveFrame.java
@@ -5,9 +5,12 @@ import importDataInfo.MoveInfo;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 import java.awt.*;
+import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -70,6 +73,52 @@ public class MoveFrame extends JFrame {
                         tableModel.addRow(rowData);
                     }
                     this.tableWQL.setModel(tableModel);
+
+                    //针对特殊的列设置排序器
+                    TableRowSorter tableRowSorter = new TableRowSorter(tableModel);
+
+                    tableRowSorter.setComparator(tableWQL.getColumn("gkey").getModelIndex(), new Comparator<String>() {
+                        @Override
+                        public int compare(String o1, String o2) {
+                            //先以@为界分裂
+                            String o1s = o1.split("@")[0];
+                            String o2s = o2.split("@")[0];
+                            String o1d = o1.split("@")[1];
+                            String o2d = o2.split("@")[1];
+                            System.out.println(o1s + " " + o1d);
+                            System.out.println(o2s + " " + o2d);
+                            if (o1s.equals(o2s)) {//前半截相同,后半截处理为Integer，比对
+                                System.out.println("前半截相同,后半截处理为Integer，比对");
+                                int o1i = Integer.valueOf(o1d);
+                                int o2i = Integer.valueOf(o2d);
+                                return Integer.compare(o1i, o2i);//对比后半截
+                            } else {
+                                return Collator.getInstance().compare(o1s, o2s);//对比前半截
+                            }
+                        }
+                    });
+
+                    Comparator<Integer> numberComparator = new Comparator<Integer>() {
+                        @Override
+                        public int compare(Integer o1, Integer o2) {//
+                            return o1 - o2;
+                        }
+                    };
+
+                    tableRowSorter.setComparator(tableWQL.getColumn("moveId").getModelIndex(), numberComparator);
+                    tableRowSorter.setComparator(tableWQL.getColumn("WORKINGSTARTTIME").getModelIndex(), numberComparator);
+
+                    tableWQL.setRowSorter(tableRowSorter);
+                    //设置排序属性
+                    RowSorter.SortKey sortKey = new RowSorter.SortKey(tableWQL.getColumn("gkey").getModelIndex(),SortOrder.ASCENDING);
+                    RowSorter.SortKey sortKey1 = new RowSorter.SortKey(tableWQL.getColumn("moveId").getModelIndex(),SortOrder.ASCENDING);
+                    RowSorter.SortKey sortKey2 = new RowSorter.SortKey(tableWQL.getColumn("WORKINGSTARTTIME").getModelIndex(),SortOrder.ASCENDING);
+                    List<RowSorter.SortKey> sortKeyList = new ArrayList<RowSorter.SortKey>();
+                    sortKeyList.add(sortKey);
+                    sortKeyList.add(sortKey1);
+                    sortKeyList.add(sortKey2);
+                    tableRowSorter.setSortKeys(sortKeyList);
+                    tableRowSorter.setSortsOnUpdates(true);
                 }
             }
         }
